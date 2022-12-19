6e2b8674527f72e49d3d3884223f7cd7fac7b06b
unknown <freelancestanislav@gmail.com>
2022-12-20 00:10:05 +0300

    loader; lazy images

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
 const loader = document.getElementById("loader")
 window.onload = () => {
-    // loader.classList.add("hide")
+    loader.classList.add("hide")
     document.body.style.overflow = "auto"
 }
